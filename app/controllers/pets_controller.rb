31cc0efc1eefe628b0e0af347aa0d16c54182e2d
sbalayan1 <balayans2014@yahoo.com>
2022-03-16 05:25:20 -0700

    work in progress

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class PetsController < ApplicationController
 
   get '/pets' do
@@ -6,11 +8,15 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owners << Owner.create(name: params[:owner][:name])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
